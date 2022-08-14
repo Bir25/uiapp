33a65822228b81eb9c10d206a87db89600e82c73
bir <birbdrgurung2020@gmail.com>
2022-08-13 22:16:30 -0600

    changes

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -6,7 +6,7 @@ class Movie{
   final int id;
   final String title;
   final String overview;
-  final String poster_vew;
+  final String poster_path;
   final String release_date;
   final String vote_average;
 
@@ -14,12 +14,12 @@ class Movie{
 required this.id,
     required this.title,
     required this.overview,
-    required this.poster_vew,
+    required this.poster_path,
     required this.release_date,
     required this.vote_average
 });
  factory Movie.fromJson(Map<String, dynamic>json)
  {
-   return Movie(id: json['id'], title: json['title'], overview: json['overview'], poster_vew: json['poster_vew'], release_date: json['release_date'], vote_average: '${json['vote_average']}');
+   return Movie(id: json['id'], title: json['title'], overview: json['overview'], poster_path: json['poster_path'], release_date: json['release_date'], vote_average: '${json['vote_average']}');
  }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:appflut/api.dart';
+import 'package:appflut/provider/movie_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 
@@ -9,7 +12,76 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container()
+    return Scaffold(appBar: AppBar(
+      toolbarHeight: 200,
+      flexibleSpace: Image.asset('assets/images/movie.png', fit: BoxFit.fill,),
+    ),
+        body:Consumer(
+          builder: (context, ref, child) {
+            final movieState = ref.watch(movieProvider);
+            return Column(
+              children: [
+                SizedBox(height: 15,),
+                Row(
+                  children: [
+                    Expanded(child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(vertical: 10,
+                                horizontal: 10),
+                            hintText: 'search movie',
+                            border: OutlineInputBorder(),
+                            // suffixIcon: Icon(Icons.add_a_photo_outlined)
+                        ),
+                      ),
+                    )),
+                    PopupMenuButton(
+                        padding: EdgeInsets.only(top: 15),
+                        icon: Icon(Icons.menu, size: 30,),
+                        onSelected: (val) {
+                          print(val);
+                        },
+                        itemBuilder: (context) {
+                          return [
+                            PopupMenuItem(
+                                value: Api.popularMoviesUrl,
+                                child: Text('popular')),
+                            PopupMenuItem(
+                                value: Api.upComingMovieUrl,
+                                child: Text('upcoming')),
+                            PopupMenuItem(
+                                value: Api.topRatedMovieUrl,
+                                child: Text('top_rated'))
+                          ];
+                        }
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Container(
+                    height: 440,
+                    child: movieState.movies.isEmpty ? Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ): GridView.builder
+                      (
+                        itemCount: movieState.movies.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          childAspectRatio: 2/3,
+                          mainAxisSpacing: 5,
+                          crossAxisSpacing: 5
+                        ),
+                        itemBuilder: (context, index){
+                          final movie = movieState.movies[index];
+                          return Image.network('https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}');
+                        }))
+              ],
+            );
+          }
+        )
     );
   }
 }
